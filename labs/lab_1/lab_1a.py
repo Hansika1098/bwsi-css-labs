2a87dd62bb11553bd5ae7c899bfea4c747db6a27
Hansika1098 <161392885+Hansika1098@users.noreply.github.com>
2026-02-16 15:01:36 -0800

    added bio to lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Hansika" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Bio: freshman from Saratoga, CA")
 
 if __name__ == "__main__":
     main()
